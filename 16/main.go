76e4caf5a9d56be59b40b10bbe4bf5886ae37f50
wallace <wallacegibbon@aliyun.com>
2018-05-04 17:52:42 +0800

    level 16 finished

--- 16/main.go
+++ 16/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"image/png"
+	"os"
 )
 
 // http://www.pythonchallenge.com/pc/return/mozart.html
@@ -9,4 +14,58 @@ import (
 // password: file
 
 func main() {
+	in, err := os.Open("mozart.gif")
+	if err != nil {
+		fmt.Println("Failed opening input image")
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create("out.png")
+	if err != nil {
+		fmt.Println("Failed opening output image")
+		return
+	}
+	defer out.Close()
+
+	img, err := gif.Decode(in)
+	if err != nil {
+		fmt.Println("Failed decoding input image")
+		return
+	}
+
+	io := image.NewNRGBA(img.Bounds())
+
+	xy := img.Bounds().Max
+	line := make([]color.Color, xy.X)
+
+	for y := 0; y < xy.Y; y++ {
+		for x := 0; x < xy.X; x++ {
+			line[x] = img.At(x, y)
+		}
+		line = shiftLine(line)
+		for x := 0; x < xy.X; x++ {
+			io.Set(x, y, line[x])
+		}
+	}
+
+	err = png.Encode(out, io)
+	if err != nil {
+		fmt.Println("Failed writing output image")
+		return
+	}
+}
+
+func shiftLine(line []color.Color) []color.Color {
+	var i int
+	for i = 0; i < len(line); i++ {
+		r, g, b, _ := line[i].RGBA()
+		r >>= 8
+		g >>= 8
+		b >>= 8
+		if r == 255 && g == 0 && b == 255 {
+			break
+		}
+	}
+	return append(line[i:], line[:i]...)
 }
---
+++ 17/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// http://www.pythonchallenge.com/pc/return/romance.html
+// username: huge
+// password: file
+
+func main() {
+}
