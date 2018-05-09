4307cdb8d73cf8f6c793469e675a422af90e44b0
wallace <wallacegibbon@aliyun.com>
2018-05-09 15:29:51 +0800

    level 17 do not rely on curl anymore

--- 17/main.go
+++ 17/main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 )
 
@@ -18,18 +19,22 @@ import (
 const baseUrl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
 
 func main() {
-	//phase1()
-	phase2()
+	var err error
+	//err = phase1()
+	//err = phase2()
+	err = phase3()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-func phase1() {
+func phase1() error {
 	var next = "12345"
 	var r string
 	for {
 		d, c, err := getutil.Get(baseUrl, next)
 		if err != nil {
-			fmt.Println("Failed get link", next)
-			break
+			return err
 		}
 		fmt.Println("\t", d)
 		r += c
@@ -38,37 +43,45 @@ func phase1() {
 			`the next busynothing is (\d+)`)
 
 		if err != nil {
-			fmt.Println("Failed parse:", err)
-			break
+			return err
 		}
 	}
 	u, err := url.QueryUnescape(r)
 	if err != nil {
-		fmt.Println("unescape error:", err)
-		return
+		return err
 	}
 	//fmt.Println(u)
 	t, err := decode(u)
 	if err != nil {
-		fmt.Println("bzip2 decode error:", err)
-		return
+		return err
 	}
 
 	fmt.Println(t)
+	return nil
 }
 
-func phase2() {
+func phase2() error {
 	buf, err := phonebook.Req("phone", "Leopold")
 	if err != nil {
-		fmt.Println("Failed request:", err)
-		return
+		return err
 	}
 	fmt.Println(buf)
+	return nil
 }
 
-func phase3() {
-	// Just use curl:
-	// curl http://www.pythonchallenge.com/pc/stuff/violin.php -H 'Cookie: info=the flowers are on their way'
+func phase3() error {
+	req, err := http.NewRequest("GET", "http://www.pythonchallenge.com/pc/stuff/violin.php", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Cookie", "info=the flowers are on their way")
+	cli := http.Client{}
+	resp, err := cli.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Write(os.Stdout)
+	return nil
 }
 
 func decode(s string) (string, error) {
