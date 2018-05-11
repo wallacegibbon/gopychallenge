86dc50917c16c7c70d00c5267dac4711e58d197a
wallace <wallacegibbon@aliyun.com>
2018-05-11 11:53:05 +0800

    some code for level 24, but not finished yet

--- 24/main.go
+++ 24/main.go
@@ -6,7 +6,30 @@ package main
 
 import (
 	"fmt"
+	"image/png"
+	"os"
 )
 
 func main() {
+	f, err := os.Open("maze.png")
+	if err != nil {
+		fmt.Println("Failed opening input image")
+		return
+	}
+	defer f.Close()
+
+	o, err := os.Create("out.png")
+	if err != nil {
+		fmt.Println("Failed opening output image")
+		return
+	}
+	defer o.Close()
+
+	img, err := png.Decode(f)
+	if err != nil {
+		fmt.Println("Failed decoding image")
+		return
+	}
+
+	png.Encode(o, img)
 }
